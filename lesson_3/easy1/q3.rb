620882bf4dbc124425a1c22853635723eb269cf4
Jensen Taylor <96322374+JensenGH@users.noreply.github.com>
2022-07-24 17:15:29 -0700

    Delete lesson_3/easy1 directory

--- lesson_3/easy1/q3.rb
+++
@@ -1,6 +0,0 @@
-# Replace the word "important" with "urgent" in this string.
-
-advice = "Few things in life are as imporant as house training your pet dinosaur."
-
-# Solution:
-advice.gsub!("imporant", "urgent")
