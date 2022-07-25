d6fde672fb2cccf0a3aa65ca23b01db3d530b619
Jensen Taylor <96322374+JensenGH@users.noreply.github.com>
2022-07-24 17:15:38 -0700

    Delete lesson_3/easy2 directory

--- lesson_3/easy2/q5.rb
+++
@@ -1,9 +0,0 @@
-=begin
-  
-Show an easier way to write this array:
-
-flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
-
-=end
-
-flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
