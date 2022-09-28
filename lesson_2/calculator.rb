e3f13e879897dcc0da28466398e04b7ecbf30fe2
Jensen Taylor <78d9f80a@slack.launchschool.com>
2022-09-28 11:26:27 -0700

    Re-factored calculator program

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,7 +1,12 @@
-# ask the user for two numbers
-# asks the user for an operation to perform
-# perform the operation on the two numbers
-# output the result
+# Creating a calculator application
+
+# General flow:
+# Ask user for two numbers
+# Ask the user for an operator to perform
+# Perform the operation on the two numbers
+# Output the result
+
+LANGUAGE = 'en'
 
 require 'yaml'
 MESSAGES = YAML.load_file('calculator_messages.yml')
@@ -11,115 +16,119 @@ def messages(message, lang='en')
 end
 
 def prompt(message)
-  puts "=> #{message}"
+  Kernel.puts("=> #{message}")
 end
 
-def integer?(input)
-  input.to_i().to_s() == input
+def integer?(num) # validating integer
+  num.to_i().to_s() == num
 end
 
-def float?(input)
-  input.to_f().to_s() == input
+def float?(num) # validating float
+  num.to_f().to_s() == num
 end
 
-def operation_to_message(op)
-  word = case op
-         when '1'
-           'Adding'
-         when '2'
-           'Subtracting'
-         when '3'
-           'Multiplying'
-         when '4'
-           'Dividing'
-         end
-  word
+def valid_number?(num) # validates a valid number
+  integer?(num) || float?(num)
 end
 
-system("clear")
+def operation_to_message(operation)
+  return_value = case operation
+                 when '1'
+                   'Adding'
+                 when '2'
+                   'Subtracting'
+                 when '3'
+                   'Multiplying'
+                 when '4'
+                   'Dividing'
+                 end
 
-prompt(messages('welcome'))
+  system("clear") # clearing the screen before starting calculator
+
+  # We can write code here after the case statement, if needed.
+
+  return_value # returning the value of case
+end
+
+prompt(messages('welcome', LANGUAGE))
 
 name = ''
 loop do
   name = Kernel.gets().chomp()
 
   if name.empty?()
-    prompt(messages('valid_name'))
+    prompt(messages('valid_name', LANGUAGE))
   else
     break
   end
 end
 
-prompt("Hi #{name}!")
+prompt("Hi, #{name}!")
 
 loop do # main loop
-  first_number = ''
+  number1 = ""
   loop do
-    prompt(messages('first_number'))
-    first_number = Kernel.gets().chomp()
+    prompt(messages('first_number', LANGUAGE))
+    number1 = Kernel.gets().chomp()
 
-    if integer?(first_number) || float?(first_number)
+    if valid_number?(number1)
       break
     else
-      prompt(messages('valid_number'))
+      prompt(messages('valid_number', LANGUAGE))
     end
   end
 
-  second_number = ''
+  number2 = ""
   loop do
-    prompt(messages('second_number'))
-    second_number = Kernel.gets().chomp()
+    prompt(messages('second_number', LANGUAGE))
+    number2 = Kernel.gets().chomp()
 
-    if integer?(second_number) || float?(second_number)
+    if valid_number?(number2)
       break
     else
-      prompt(messages('valid_number'))
+      prompt(messages('valid_number', LANGUAGE))
     end
   end
 
   operator_prompt = <<-MSG
-    What operation would you like to perform?
-    1) add
-    2) subtract
-    3) multiply
-    4) divide
+  What operation would you like to perform?
+  1) add
+  2) subtract
+  3) multiply
+  4) divide
   MSG
 
-  prompt(operator_prompt)
+  prompt(messages('operator_prompt', LANGUAGE))
 
-  operator = ''
+  operator = ""
   loop do
     operator = Kernel.gets().chomp()
 
     if %w(1 2 3 4).include?(operator)
       break
     else
-      prompt(messages('operator_prompt'))
+      prompt(messages('operator_prompt', LANGUAGE))
     end
   end
 
-  prompt("#{operation_to_message(operator)} the two nummbers...")
+  prompt("#{operation_to_message(operator)} the two numbers...")
 
   result = case operator
            when '1'
-             first_number.to_f() + second_number.to_f()
+             number1.to_f() + number2.to_f()
            when '2'
-             first_number.to_f() - second_number.to_f()
+             number1.to_f() - number2.to_f()
            when '3'
-             first_number.to_f() * second_number.to_f()
+             number1.to_f() * number2.to_f()
            when '4'
-             first_number.to_f() / second_number.to_f()
+             number1.to_f() / number2.to_f()
            end
 
   prompt("The result is #{result}")
 
-  prompt(messages('another_calc'))
+  prompt(messages('another_calc', LANGUAGE))
   answer = Kernel.gets().chomp()
-
-  system("clear")
-
-  break unless answer.downcase().start_with?('y')
+  break unless answer.downcase().start_with?("y")
 end
 
-prompt(messages('thanks'))
+prompt(messages('thanks', LANGUAGE))
