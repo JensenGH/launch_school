5d1483405070e01126a9933c3b8f1ab88859f1d0
Jensen Taylor <78d9f80a@slack.launchschool.com>
2022-09-28 13:49:51 -0700

    Re-worked the Mortgage Calculator with yml file

--- lesson_2/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator.rb
@@ -1,87 +1,77 @@
-=begin
-
-Welcome the user to your program
-Prompt user for loan amount
-Prompt user for Annual Percentage Rate (APR)
-Prompt user for loan duration
-
-Calculate monthly interest rate
-Calculate loan duration in months
-Calculate a monthly payment
-
-=end
+LANGUAGE = 'en'
 
 require 'yaml'
 MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
 
+def messages(message, lang='en')
+  MESSAGES[lang][message]
+end
+
 def prompt(message)
-  Kernel.puts "=> #{message}"
+  puts("=> #{message}")
 end
 
-def not_valid_number(input)
-  input.empty?() || input.to_f() < 0
+def invalid_input(input)
+  input.empty? || input.to_f < 0
 end
 
-system ("clear")
+system("clear")
 
 loop do
-  prompt MESSAGES['welcome']
-  prompt MESSAGES['loan_amount']
+  prompt(messages('welcome', LANGUAGE))
+  prompt(messages('loan_amount', LANGUAGE))
 
-  amount = ''
+  loan_amount = ''
   loop do
-    amount = Kernel.gets().chomp()
+    loan_amount = gets.chomp
 
-    if not_valid_number(amount)
-      prompt MESSAGES['valid_number']
+    if loan_amount.empty? ||  loan_amount.to_f < 0
+      prompt(messages('valid_number', LANGUAGE))
     else
       break
     end
   end
 
-  prompt MESSAGES['interest_rate']
+  prompt(messages('interest_rate', LANGUAGE))
 
   interest_rate = ''
   loop do
-    interest_rate = Kernel.gets().chomp()
+    interest_rate = gets.chomp
 
-    if not_valid_number(interest_rate)
-      prompt MESSAGES['valid_number']
+    if interest_rate.empty? || interest_rate.to_f < 0
+      prompt(messages('interest_rate', LANGUAGE))
     else
       break
     end
   end
 
-  prompt MESSAGES['loan_duration']
+  prompt(messages('loan_duration', LANGUAGE))
 
-  years = ''
+  loan_duration = ''
   loop do
-    years = Kernel.gets().chomp()
+    loan_duration = gets.chomp
 
-    if not_valid_number(years)
-      prompt MESSAGES['valid_number']
+    if loan_duration.empty? || loan_duration.to_f < 0
+      prompt(messages('valid_number', LANGUAGE))
     else
       break
     end
   end
 
-  annual_interest_rate = interest_rate.to_f() / 100
+  annual_interest_rate = interest_rate.to_f / 100
   monthly_interest_rate = annual_interest_rate / 12
-  months = years.to_i() * 12
+  months = loan_duration.to_i * 12
 
-  monthly_payment = amount.to_f() *
-                    (monthly_interest_rate / (1 -
-                    (1 + monthly_interest_rate)**(-months)))
+  monthly_payment = loan_amount.to_f * 
+                    (monthly_interest_rate / 
+                    (1 - (1 + monthly_interest_rate.to_f)**(-months)))
 
   prompt("Your monthly payment is: $#{format('%.2f', monthly_payment)}")
 
-  prompt MESSAGES['another_calc']
-  answer = Kernel.gets().chomp()
+  prompt(messages('another_calc', LANGUAGE))
+  answer = gets.chomp
 
-  system ("clear")
-
-  break unless answer.downcase().start_with?('y')
+  break unless answer.downcase.start_with?('Y')
 end
 
-prompt MESSAGES['thank_you']
-prompt MESSAGES['good_bye']
+prompt(messages('thank_you', LANGUAGE))
