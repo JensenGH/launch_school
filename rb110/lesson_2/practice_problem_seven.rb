e4b88f468eebb040bdb061d5842cd9f51974c7dd
Jensen Taylor <78d9f80a@slack.launchschool.com>
2023-08-26 08:08:31 -0700

    Corrected practice problem 7

--- rb110/lesson_2/practice_problem_seven.rb
+++ rb110/lesson_2/practice_problem_seven.rb
@@ -7,8 +7,8 @@ a = 2
 b = [5, 8]
 arr = [a, b] # => [2, [5, 8]]
 
-arr[0] = 4 # => [4, [5, 8]] a is now assigned to the integer 4
-arr[1][0] -= a # => [4, [3, 8]]
+arr[0] = 4 # => 4 element at index 0 of `arr` is now 4
+arr[1][0] -= a # => 3 element at index 0 of the inner array is now 3
 
 p a # => 2
 p b # => [3, 8]
